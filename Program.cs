1618d102c698f84e97ccddedacede9184ee88bc9
taniaghosh16 <99164260+taniaghosh16@users.noreply.github.com>
2022-08-17 17:17:14 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,30 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ReverseStringg
+namespace List
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            string str = "", reverse = "";
-            int Length = 0;
-            Console.WriteLine("Enter a Word");
-            //Getting String(word) from Console  
-            str = Console.ReadLine();
-            //Calculate length of string str  
-            Length = str.Length - 1;
-            while (Length >= 0)
+            var student = new List<clsroom>()
             {
-                reverse = reverse + str[Length];
-                Length--;
-            }
-            //Displaying the reverse word  
-            Console.WriteLine("Reverse word is {0}", reverse);
-            Console.ReadLine();
+                new clsroom(){ stud_id=1,stud_Fname="Tania",stud_lname="Ghosh",Roll_no=65},
+                new clsroom(){ stud_id=2,stud_Fname="Poulami",stud_lname="Dhar",Roll_no=15},
+                new clsroom(){ stud_id=3,stud_Fname="suvo",stud_lname="dey",Roll_no=89},
+                new clsroom(){ stud_id=4,stud_Fname="nishan",stud_lname="Mal",Roll_no=68},
+                new clsroom(){ stud_id=5,stud_Fname="souvik",stud_lname="ghosh",Roll_no=14},
+                new clsroom(){ stud_id=6,stud_Fname="Debjit",stud_lname="Purkaite",Roll_no=89},
+
+            };
+            var Rollno = from rno in student
+                         where rno.stud_Fname == "Tania"
+                         select rno;
+            foreach (var item in Rollno)
+                Console.WriteLine(item.stud_Fname +" " + item.Roll_no);
+
+
         }
     }
+    public class clsroom
+    {
+        public int stud_id { get; set; }
+        public string stud_Fname { get; set; }
+        public string stud_lname { get; set; }
+        public int Roll_no { get; set; }
+    }
+
 }
+
+
+
+
+
