c9ff34f895634147ec0bbd80cc5a0a4735f0b779
taniaghosh16 <99164260+taniaghosh16@users.noreply.github.com>
2022-08-17 17:17:55 +0530

    Add files via upload

---
+++ Program (1).cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace ConsoleApp
+{
+    class tania
+    {
+        public delegate void Taniadelegate(string name);
+        public delegate void Multidelegate(int x, int y);
+        public void Multi(int x, int y)
+        {
+            Console.WriteLine(x * y);
+
+        }
+        public void name(string name)
+        {
+            Console.WriteLine("my name is" + " " + name);
+
+        }
+        static void Main(string[] args)
+        {
+            tania d = new tania();
+            Taniadelegate obj1 = new Taniadelegate(d.name);
+            Multidelegate obj2 = new Multidelegate(d.Multi);
+            obj1("Tania");
+            obj2(20, 20);
+
+        }
+
+    }
+
+}
