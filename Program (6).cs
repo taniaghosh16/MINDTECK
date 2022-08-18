4f224c596ddfc81c91007736f225e3252b7efe89
taniaghosh16 <99164260+taniaghosh16@users.noreply.github.com>
2022-08-18 18:18:05 +0530

    Add files via upload

---
+++ Program (6).cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Polymorphism
+{
+    public class Shape
+    {
+        public virtual void draw()
+        {
+            Console.WriteLine("drawing...");
+        }
+    }
+    public class Rectangle : Shape
+    {
+        public override void draw()
+        {
+            Console.WriteLine("drawing rectangle...");
+        }
+
+    }
+    public class Circle : Shape
+    {
+        public override void draw()
+        {
+            Console.WriteLine("drawing circle...");
+        }
+
+    }
+    public class TestPolymorphism
+    {
+        public static void Main()
+        {
+            Shape s;
+            s = new Shape();
+            s.draw();
+            s = new Rectangle();
+            s.draw();
+            s = new Circle();
+            s.draw();
+
+        }
+    }
+}
