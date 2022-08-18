08350291a1bfaa6d686728a06c1da466959826f0
taniaghosh16 <99164260+taniaghosh16@users.noreply.github.com>
2022-08-18 18:17:36 +0530

    Add files via upload

---
+++ Program (5).cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Inheritance
+{
+
+
+    class Animal
+    {
+
+        public string name;
+
+        public void display()
+        {
+            Console.WriteLine("I am an animal");
+        }
+
+    }
+
+
+    class Cat : Animal
+    {
+
+        public void getName()
+        {
+            Console.WriteLine("My name is " + name);
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            Cat C = new Cat();
+
+
+            C.name = "Max";
+            C.display();
+
+
+            C.getName();
+
+            Console.ReadLine();
+        }
+
+    }
+}
