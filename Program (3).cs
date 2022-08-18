89e1fd677ec6dfac5a643e720b692916bc5f46b0
taniaghosh16 <99164260+taniaghosh16@users.noreply.github.com>
2022-08-18 18:16:35 +0530

    Add files via upload

---
+++ Program (3).cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace MyConsoleApp
+{
+    class vehicle
+    {
+        string Brand_Name;
+        public void Quality()
+        {
+            Console.WriteLine("Quality is so good");
+        }
+        public void Price(int x)
+        {
+            Console.WriteLine("The price of the Vehicle is" + "" + "" + x);
+        }
+        public void Customer_Support(string name)
+        {
+            Console.WriteLine("Customer support is " + "" + name);
+        }
+        static void Main()
+        {
+            vehicle v = new vehicle();
+            v.Brand_Name = "Royal Enfield";
+            v.Quality();
+            v.Price(120000);
+            v.Customer_Support("Good");
+            Console.WriteLine(v.Brand_Name = "Royal Enfield");
+            Console.ReadLine();
+
+
+        }
+    }
+}
