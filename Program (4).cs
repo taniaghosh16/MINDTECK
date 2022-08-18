08832618823380dd3241e219a425f380f421bfd6
taniaghosh16 <99164260+taniaghosh16@users.noreply.github.com>
2022-08-18 18:17:08 +0530

    Add files via upload

---
+++ Program (4).cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+namespace AccessModifiers
+{
+
+    class Student
+    {
+        public int rollNo;
+        public string name;
+
+
+        public Student(int r, string n)
+        {
+            rollNo = r;
+            name = n;
+        }
+
+
+        public int getRollNo()
+        {
+            return rollNo;
+        }
+        public string getName()
+        {
+            return name;
+        }
+    }
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            Student S = new Student(1, "Tania");
+
+            Console.WriteLine("Roll number: {0}", S.rollNo);
+            Console.WriteLine("Name: {0}", S.name);
+
+            Console.WriteLine();
+
+
+            Console.WriteLine("Roll number: {0}", S.getRollNo());
+            Console.WriteLine("Name: {0}", S.getName());
+        }
+    }
+}
