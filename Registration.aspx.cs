9fdca679036f47b04ddfb4e10a764ce559b5356e
taniaghosh16 <99164260+taniaghosh16@users.noreply.github.com>
2022-08-29 17:59:54 +0530

    Add files via upload

---
+++ Registration.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Registration
+{
+    public partial class Registration : System.Web.UI.Page
+    {
+        SqlConnection SqlCon = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        string myConnectionString = ConfigurationManager.ConnectionStrings["StrCon"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlCon.ConnectionString = (myConnectionString);
+                SqlCon.Open();
+                com.Connection = SqlCon;
+                com.CommandText = "INSERT INTO [dbo].[Registration Details] (Name,Mobile,EmailId) VALUES (@Name,@Mobile,@Email)";
+                com.Parameters.AddWithValue("@Name", textName.Text.Trim());
+                com.Parameters.AddWithValue("@Mobile", textMobilenumber.Text.Trim());
+                com.Parameters.AddWithValue("@Email", textMailId.Text.Trim());
+                com.ExecuteNonQuery();
+                SqlCon.Close();
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+
+            }
+
+        }
+    }
+}
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApplication2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string gender;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        } 
+
+        
+
+        protected void btnReg_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=LAPTOP-D7A8C66S;Initial Catalog=userreg;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("INSERT INTO [dbo].[userreg]\r\n           ([name]\r\n           ,[address]\r\n           ,[gender]\r\n           ,[phone]\r\n           ,[email]\r\n           ,[username]\r\n           ,[password])\r\n     VALUES\r\n           ('" +txtName.Text+ "', '" +txtAdd.Text+ "', '" +gender+ "', '" +txtPhone.Text+ "', '" +txtEmail.Text+ "', '" +txtUser.Text+ "', '" +txtPass.Text+"')", con);
+                con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Response.Write("<script>alert('user is registered successfully')</script>");
+        }
+
+        protected void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            gender="male";
+        }
+
+        protected void RadioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            gender="female";
+        }
+
+        protected void RadioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            gender="others";
+        }
+    }
+}
